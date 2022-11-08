6dd766facf3437e01ed7049ee1e62c4f3f95e059
dylan <dylanfruit@outlook.fr>
2022-11-08 10:13:27 +0100

    Ajout de classe active dans le header pour afficher sur quelle page se trouve le user

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,17 +1,31 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import logobg from "../../assets/image/logobg.png";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+  const location = useLocation();
+
+  const { pathname } = location;
+
+  const splitLocation = pathname.split("/");
+
   return (
     <header className="header">
       <div className="header-logo-nav">
         <img src={logobg} alt="logo of Wealth Health" />
         <NavLink to="/employee-list">
-          <p className="btn">View current employees</p>
+          <p
+            id="btn"
+            className={splitLocation[1] === "employee-list" ? "isActive" : ""}
+          >
+            View current employees
+          </p>
         </NavLink>
         <NavLink to="/">
-          <p className="btn">Create employee</p>
+          <p id="btn" className={splitLocation[1] === "" ? "isActive" : ""}>
+            Create employee
+          </p>
         </NavLink>
       </div>
       <h1>HRnet</h1>
